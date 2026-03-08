9e7a1ae952c496b6dfbf5a5f2101076e71c366d8
Shekhar2304 <souravshekhar22400@gmail.com>
2026-03-08 22:45:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-super-secret-key-change-in-production'
     SQLALCHEMY_DATABASE_URI = "sqlite:///acid_to_amp.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
